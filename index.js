78577cb9aef5823b039b2ddf3f52493d7edad3d6
nkunic <natasa.kunic@gmail.com>
2022-04-23 16:43:53 +0200

    2 files modified

--- index.js
+++ index.js
@@ -9,33 +9,34 @@ var accBtn = document.querySelector('#accBtn');
 var addAccBtn = document.querySelector('#addAccBtn');
 var editDeleteBtn = document.querySelector('#editDeleteBtn');
 
-var mainView = document.querySelector('#mainView');
 var mainBody = document.querySelector('#mainBody');
+var mainView = document.querySelector('#mainView');
+var formView = document.querySelector('#formView');
 
 var db = [
   {
-    id: "1",
-    name: "Mark Otto",
-    deposit: "12000",
-    cCard: "Visa"
+    id: '1',
+    name: 'Mark Otto',
+    deposit: '12000',
+    cCard: 'Visa',
   },
   {
-    id: "2",
-    name: "Jacob Thornton",
-    deposit: "15000",
-    cCard: "Mastercard"
+    id: '2',
+    name: 'Jacob Thornton',
+    deposit: '15000',
+    cCard: 'Mastercard',
   },
   {
-    id: "3",
-    name: "Larry Bird",
-    deposit: "100000",
-    cCard: "American express"
-  }
+    id: '3',
+    name: 'Larry Bird',
+    deposit: '100000',
+    cCard: 'American express',
+  },
 ];
 
 function createTable() {
   var text = '';
-  for(var i = 0; i < db.length; i++) {
+  for (var i = 0; i < db.length; i++) {
     text += '<tr>';
     text += '<td>' + db[i].id + '</td>';
     text += '<td>' + db[i].name + '</td>';
