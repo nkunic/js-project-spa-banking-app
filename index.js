e5cdc401a66fb66ec876eb5018e2a0a16fb26125
nkunic <natasa.kunic@gmail.com>
2022-04-23 13:27:36 +0200

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,46 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import './css/style.css';
 
 // Write Javascript code!
+
+var accBtn = document.querySelector('#accBtn');
+var addAccBtn = document.querySelector('#addAccBtn');
+var editDeleteBtn = document.querySelector('#editDeleteBtn');
+
+var mainView = document.querySelector('#mainView');
+var mainBody = document.querySelector('#mainBody');
+
+var db = [
+  {
+    id: "1",
+    name: "Mark Otto",
+    deposit: "12000",
+    cCard: "Visa"
+  },
+  {
+    id: "2",
+    name: "Jacob Thornton",
+    deposit: "15000",
+    cCard: "Mastercard"
+  },
+  {
+    id: "3",
+    name: "Larry Bird",
+    deposit: "100000",
+    cCard: "American express"
+  }
+];
+
+function createTable() {
+  var text = '';
+  for(var i = 0; i < db.length; i++) {
+    text += '<tr>';
+    text += '<td> + db[i].id + </td>';
+    text += '<td> + db[i].name + </td>';
+    text += '<td> + db[i].deposit + </td>';
+    text += '<td> + db[i].cCard + </td>';
+    text += '</tr>';
+  }
+  mainBody.innerHTML = text;
+}
+
+createTable();
