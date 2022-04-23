a373044177f10d464458034578465e6538bcffad
nkunic <natasa.kunic@gmail.com>
2022-04-23 11:44:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-project-spa-banking-app-nk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-9npjoj)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './css/style.css';
+
+// Write Javascript code!
